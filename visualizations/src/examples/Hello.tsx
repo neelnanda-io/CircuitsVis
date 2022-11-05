70516d04edb38e370ff78d4e7642074481d2d740
Alan <41682961+alan-cooney@users.noreply.github.com>
2022-11-05 11:25:50 +0000

    Move to new folder structure

---
+++ visualizations/src/examples/Hello.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+/**
+ * React TypeScript Example
+ */
+export default function Hello({ name }: { name: string }) {
+  return <p>Hello, {name}!</p>;
+}
---
+++ visualizations/src/index.ts
@@ -0,0 +1,4 @@
+import AttentionPatterns  from "./attention/AttentionPatterns";
+import Hello from "./examples/Hello"
+
+export {AttentionPatterns, Hello}
