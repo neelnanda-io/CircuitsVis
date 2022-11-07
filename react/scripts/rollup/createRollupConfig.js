d2a1295aa298f873d4e316d3ffa042625f2c7ef8
Alan <41682961+alan-cooney@users.noreply.github.com>
2022-11-07 09:24:32 +0000

    Partially fix rollup with node modules include

--- react/scripts/rollup/createRollupConfig.js
+++ react/scripts/rollup/createRollupConfig.js
@@ -4,13 +4,50 @@ import external from "rollup-plugin-peer-deps-external";
 import sourcemaps from "rollup-plugin-sourcemaps";
 import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-export function createRollupConfig(options, callback) {
+export function createRollupConfig(options) {
   const { name } = options;
   // A file with the extension ".mjs" will always be treated as ESM, even when pkg.type is "commonjs" (the default)
   // https://nodejs.org/docs/latest/api/packages.html#packages_determining_module_system
   const extName = options.format === "esm" ? "mjs" : "js";
   const outputName = `dist/${[name, options.format, extName].join(".")}`;
+
+  const plugins = [
+    external(),
+    typescript({
+      tsconfig: options.tsconfig,
+      clean: true,
+      exclude: [
+        "**/__tests__",
+        "**/*.test.ts",
+        "**/*.stories.ts",
+        "/scripts/**/*"
+      ]
+    }),
+    sourcemaps(),
+    nodeResolve({})
+  ];
+
+  if (options.format === "umd") {
+    plugins.push(
+      commonjs({
+        include: "/node_modules/",
+        defaultIsModuleExports: true
+      })
+    );
+  }
+
+  if (options.format === "esm") {
+    plugins.push(
+      terser({
+        output: { comments: false },
+        compress: {
+          drop_console: true
+        }
+      })
+    );
+  }
 
   const config = {
     input: options.input,
@@ -22,32 +59,8 @@ export function createRollupConfig(options, callback) {
       globals: { react: "React" },
       exports: "named"
     },
-    plugins: [
-      external(),
-      typescript({
-        tsconfig: options.tsconfig,
-        clean: true,
-        exclude: [
-          "**/__tests__",
-          "**/*.test.ts",
-          "**/*.stories.ts",
-          "/scripts/**/*"
-        ]
-      }),
-      options.format === "umd" &&
-      commonjs({
-        include: /\/node_modules\//
-      }),
-      sourcemaps(),
-      options.format !== "esm" &&
-      terser({
-        output: { comments: false },
-        compress: {
-          drop_console: true
-        }
-      })
-    ].filter(Boolean)
+    plugins
   };
 
-  return callback ? callback(config) : config;
+  return config;
 }
--- react/scripts/rollup/rollup.config.js
+++ react/scripts/rollup/rollup.config.js
@@ -1,20 +1,20 @@
-import pkg from '../../package.json';
+import pkg from "../../package.json";
 
-import { createRollupConfig } from './createRollupConfig';
+import { createRollupConfig } from "./createRollupConfig";
 
-const name = 'index';
+const name = "index";
 const options = [
   {
     name,
-    format: 'cjs',
-    input: pkg.source,
+    format: "cjs",
+    input: pkg.source
   },
-  { name, format: 'esm', input: pkg.source },
+  { name, format: "esm", input: pkg.source },
   {
     name,
-    format: 'umd',
-    input: pkg.source,
-  },
+    format: "umd",
+    input: pkg.source
+  }
 ];
 
 export default options.map((option) => createRollupConfig(option));
